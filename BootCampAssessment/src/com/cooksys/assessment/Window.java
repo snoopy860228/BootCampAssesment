c3558cfc61eb7878468169d936547e3a64eb531c
Jorge <Jorge@Major>
2016-08-11 22:40:36 -0400

    Second

--- BootCampAssessment/src/com/cooksys/assessment/Window.java
+++ BootCampAssessment/src/com/cooksys/assessment/Window.java
@@ -4,12 +4,36 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
 import java.awt.BorderLayout;
+
 import javax.swing.JButton;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+
+import javax.swing.DefaultListModel;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JMenu;
+import javax.swing.JList;
+import javax.swing.ListSelectionModel;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.FlowLayout;
+import javax.swing.BoxLayout;
+import java.awt.CardLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.factories.FormFactory;
+import com.jgoodies.forms.layout.RowSpec;
+import java.awt.GridLayout;
 
 public class Window {
 
@@ -45,9 +69,113 @@ public class Window {
 	 * will generate its code.
 	 */
 	public void initialize() {
-		frame = new JFrame();
+		frame = new JFrame("PC Part Builder");
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("File");
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("Load");
+		mntmNewMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				load();
+			}
+		});
+		mnNewMenu.add(mntmNewMenuItem);
+		
+		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Save");
+		mntmNewMenuItem_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				save();
+			}
+		});
+		mnNewMenu.add(mntmNewMenuItem_2);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Exit");
+		mntmNewMenuItem_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		mnNewMenu.add(mntmNewMenuItem_1);
+		
+        final DefaultListModel pcPartsNames = new DefaultListModel();
+		
+		pcPartsNames.addElement("Case");
+		pcPartsNames.addElement("Motherboard");
+		pcPartsNames.addElement("CPU");
+		pcPartsNames.addElement("GPU");
+		pcPartsNames.addElement("PSU");
+		pcPartsNames.addElement("RAM");
+		pcPartsNames.addElement("HDD");
+		
+		final JList list = new JList(pcPartsNames);
+		list.setVisibleRowCount(3);
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		list.setSelectedIndex(0);
+		
+		final DefaultListModel pcPartsNames1 = new DefaultListModel();
+		
+		JPanel panel = new JPanel();
+		
+		JButton addButton = new JButton(">>");
+		addButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				pcPartsNames1.addElement(list.getSelectedValue());
+				pcPartsNames.removeElement(list.getSelectedValue());
+			}
+		});
+		
+		final JList list_1 = new JList(pcPartsNames1);
+		list_1.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		list_1.setSelectedIndex(0);
+		frame.getContentPane().setLayout(new GridLayout(0, 3, 0, 0));
+		frame.getContentPane().add(list);
+		frame.getContentPane().add(panel);
+		
+		JButton removeButton = new JButton("<<");
+		removeButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				pcPartsNames1.removeElement(list_1.getSelectedValue());
+			}
+		});
+		
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addGap(47)
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addComponent(removeButton)
+							.addContainerGap())
+						.addGroup(gl_panel.createSequentialGroup()
+							.addComponent(addButton, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+							.addGap(48))))
+		);
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addGap(80)
+					.addComponent(addButton, GroupLayout.PREFERRED_SIZE, 31, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(removeButton, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
+					.addGap(88))
+		);
+		panel.setLayout(gl_panel);
+		frame.getContentPane().add(list_1);
+	}
+	
+	public void save(){
+		//TODO		
+	}
+	
+    public void load(){
+    	//TODO
 	}
 
 }
